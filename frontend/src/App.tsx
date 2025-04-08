5729338a632b6a15494963c28baa30750e5f0cf3
ellalauritzen <lauri4@byu.edu>
2025-04-08 15:56:46 -0600

    Added dynamic header settings

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,8 +11,8 @@ import Footer from "./components/GlobalFooter";
 function App() {
   return (
     <>
-      <Header />
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
           <Route path="/admin" element={<Admin />}></Route>
