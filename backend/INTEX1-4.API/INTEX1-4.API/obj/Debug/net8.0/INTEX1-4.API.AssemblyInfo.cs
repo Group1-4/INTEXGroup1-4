b4fdf35e07ace6820afb48a1d3e3e60447b6d5e8
Jeffery YaTES <jefferybyates@gmail.com>
2025-04-08 10:52:06 -0600

    privacy.tsx and privacy.css with baby changes in index.css

--- backend/INTEX1-4.API/INTEX1-4.API/obj/Debug/net8.0/INTEX1-4.API.AssemblyInfo.cs
+++ backend/INTEX1-4.API/INTEX1-4.API/obj/Debug/net8.0/INTEX1-4.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("INTEX1-4.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cf5ab44e90451642c881ee81db925d52245c2ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ae5d0a6b4131fca45018135920fbb92c1eb7e28")]
 [assembly: System.Reflection.AssemblyProductAttribute("INTEX1-4.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("INTEX1-4.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
