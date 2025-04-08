4104fca3704b45a0c6c8b9a87d6fa90da6667b19
Cooper <coopdav4@gmail.com>
2025-04-07 20:27:39 -0600

    so i can pull

--- backend/INTEX1-4.API/INTEX1-4.API/obj/Debug/net8.0/INTEX1-4.API.AssemblyInfo.cs
+++ backend/INTEX1-4.API/INTEX1-4.API/obj/Debug/net8.0/INTEX1-4.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("INTEX1-4.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d5bf40ea954073db5b1023b9934d5bebbf25376")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e19fbc75711b8c0dab60a6147c8d90846615d8f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("INTEX1-4.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("INTEX1-4.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
