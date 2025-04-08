70854e99cf58f203e625159f5c9314381845b9ff
ccloud8939 <cclo8939@byu.edu>
2025-04-08 17:56:43 -0600

    commit

--- backend/INTEX1-4.API/INTEX1-4.API/obj/Debug/net8.0/INTEX1-4.API.AssemblyInfo.cs
+++ backend/INTEX1-4.API/INTEX1-4.API/obj/Debug/net8.0/INTEX1-4.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("INTEX1-4.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0948e34e7914a9511fcbd59de207084b4f3c192a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66fe460aa87f41ffb7e839a57b0c4cb02e85237c")]
 [assembly: System.Reflection.AssemblyProductAttribute("INTEX1-4.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("INTEX1-4.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
