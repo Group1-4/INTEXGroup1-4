45fe7449f1af17e6439520495aa705bf73d621e7
Cooper <coopdav4@gmail.com>
2025-04-10 07:59:03 -0600

    logout url update

--- frontend/src/api/MoviesAPI.ts
+++ frontend/src/api/MoviesAPI.ts
@@ -248,7 +248,7 @@ export async function fetchUserMovieDetails(movieId: string) {
 //log out user and send to home page
 // src/api/AuthAPI.ts
 export const logoutUser = async () => {
-  await fetch("https://localhost:4000/api/auth/logout", {
+  await fetch("https://localhost:4000/logout", {
     method: "POST",
     credentials: "include",
   });
