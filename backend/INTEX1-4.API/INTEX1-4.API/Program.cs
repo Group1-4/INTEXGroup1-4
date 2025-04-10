92bc9218b692caea35651fe0dac1466c8b4b2272
Cooper <coopdav4@gmail.com>
2025-04-10 13:40:12 -0600

    maybe fixed it?

--- backend/INTEX1-4.API/INTEX1-4.API/Program.cs
+++ backend/INTEX1-4.API/INTEX1-4.API/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+using System.Globalization;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
@@ -75,6 +75,7 @@ builder.Services.AddCors(options =>
             .AllowCredentials();
     });
 });
+
 
 var app = builder.Build();
 
